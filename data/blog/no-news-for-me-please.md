254e20e767257375fe795fd2ab2cd4639086a270
Paarug Sethi <paarug@Paarugs-MacBook-Air.local>
2022-10-09 19:31:34 +0530

    Initial Commit

---
+++ data/blog/no-news-for-me-please.md
@@ -0,0 +1,24 @@
+---
+title: 'No news for me, please.'
+date: 2022-06-22T15:32:14Z
+lastmod: '2022-06-22'
+tags: ['news', 'social-media', 'taleb', 'mental-health']
+draft: false
+summary: 'I have stopped reading the news and I have never felt better. Here is why.'
+layout: PostSimple
+canonicalUrl: https://tailwind-nextjs-starter-blog.vercel.app/blog/no-news-for-me-please/
+externalLink: https://wordcelclub.com/paarug.sol/no-news-for-me-please/
+---
+
+I grew up being taught that having general awareness and being up to date with current events was essential to your overall development as a member of society.
+
+This sentiment stayed with me throughout my school life and into my early adulthood as well.
+
+I was always reading or learning about whatever the current “problem” was. The thought process behind that was that even if some issue doesn’t affect me directly, it was my moral responsibility to know and in fact, learn about it. Ignorance was not just an immense amount of privilege but also a political statement in itself that I’m with the oppressor. “If you have something to say, silence is a lie”.
+
+Every scam, every scandal, every crime, every protest, and every bill passed was my problem and learning and talking about it is how I fulfilled my responsibility as a “virtuous” citizen. In hindsight, social media was a big contributor to that fire as it gave me an outlet to broadcast my thoughts to the world (or, my subset of the world) which made me feel like a celebrity of sorts.
+
+What do most celebrities do on social media? They virtue signal.
+
+## But I don’t do it anymore.
+
---
+++ layouts/PostLayout.js
@@ -0,0 +1,81 @@
+import Link from '@/components/Link'
+import PageTitle from '@/components/PageTitle'
+import SectionContainer from '@/components/SectionContainer'
+import { BlogSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
+import formatDate from '@/lib/utils/formatDate'
+import Comments from '@/components/comments'
+import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+
+export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
+  const { date, title, externalLink } = frontMatter
+
+  return (
+    <SectionContainer>
+      <BlogSEO url={`${siteMetadata.siteUrl}/blog/${frontMatter.slug}`} {...frontMatter} />
+      <ScrollTopAndComment />
+      <article>
+        <div>
+          <header>
+            <div className="space-y-1 border-b border-gray-200 pb-10 text-center dark:border-gray-700">
+              <dl>
+                <div>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time dateTime={date}>{formatDate(date)}</time>
+                  </dd>
+                </div>
+              </dl>
+              <div>
+                <PageTitle>{title}</PageTitle>
+              </div>
+            </div>
+          </header>
+          <div
+            className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 "
+            style={{ gridTemplateRows: 'auto 1fr' }}
+          >
+            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
+            </div>
+            {/* <Comments frontMatter={frontMatter} /> */}
+            <div className="space-y-1 pb-10 text-center dark:border-gray-700">
+            <div>
+                <Link
+                  href={`${externalLink}`}
+                  className="text-base font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10 md:text-2xl md:leading-14 underline"
+                >
+                  Read the complete post here on Wordcel
+                </Link>
+              </div>
+            </div>
+            <footer>
+              <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
+                {prev && (
+                  <div className="pt-4 xl:pt-8">
+                    <Link
+                      href={`/blog/${prev.slug}`}
+                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                    >
+                      &larr; {prev.title}
+                    </Link>
+                  </div>
+                )}
+                {next && (
+                  <div className="pt-4 xl:pt-8">
+                    <Link
+                      href={`/blog/${next.slug}`}
+                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                    >
+                      {next.title} &rarr;
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </footer>
+          </div>
+        </div>
+      </article>
+    </SectionContainer>
+  )
+}
