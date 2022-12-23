fd675a356ba64ebc417fb4526e68cfe97871ae4e
Paarug Sethi <paarug@Paarugs-MacBook-Air.local>
2022-12-24 01:14:49 +0530

    tried again

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
     'react/prop-types': 0,
     'no-unused-vars': 0,
     'react/no-unescaped-entities': 0,
-    "react/no-unescaped-entities": "off",
     "@next/next/no-page-custom-font": "off"
   },
 }
