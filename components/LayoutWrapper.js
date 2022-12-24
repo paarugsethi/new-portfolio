04b26604b1fceea525b83f8ae35581b7647fe159
Paarug Sethi <paarug@Paarugs-MacBook-Air.local>
2022-12-24 14:56:45 +0530

    css changes to mobile

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -19,7 +19,8 @@ const LayoutWrapper = ({ children }) => {
                   <Logo />
                 </div> */}
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  // removed 'hidden' from here to add PS to mobile
+                  <div className=" h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
--- pages/index.js
+++ pages/index.js
@@ -24,19 +24,62 @@ export default function Home({ posts }) {
           <p className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-3xl md:leading-14">
             Hello. I am Paarug Sethi.
           </p>
-          <p className="text-lg leading-3 text-gray-500 dark:text-gray-400">
+          <p className="text-lg leading-6 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
           <p className="prose max-w-none pt-0 pb-3 text-base dark:prose-dark xl:col-span-2">
-            An asymmetry hunter and polymath in aspiration. I love writing, creating visual content, and learning new things like creating AI tools and web3 development that make my inner nerd happy. 
-            Either lifting, reading, writing, drinking coffee or doing BJJ right now depending on the time of the day.
+            An asymmetry hunter and polymath in aspiration. I love writing, creating visual content,
+            and learning new things like creating AI tools and web3 development that make my inner
+            nerd happy. Either lifting, reading, writing, drinking coffee or doing BJJ right now
+            depending on the time of the day.
           </p>
           <ul className="prose list-none pt-0 text-base dark:prose-dark">
-            <li>Head of Marketing and Comms at <a href="https://www.twitter.com/wordcel_club" target="_blank" rel="noopener noreferrer">Wordcel</a></li>
-            <li>Member and Head of the Host Committee at the <a href="https://www.twitter.com/SuperteamDAO" target="_blank" rel="noopener noreferrer">SuperteamDAO</a></li>
-            <li>Co-founder of the <a href="https://linktr.ee/slipperyslopes" target="_blank" rel="noopener noreferrer">Slippery Slopes Podcast</a></li>
-            <li>Writing on my <a href="https://www.wordcelclub.com/paarug.sol" target="_blank" rel="noopener noreferrer">Blog</a> and <a href="https://twitter.com/paarugsethi" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-            <li>Previously wrote content at <a href="https://twitter.com/TimeswapLabs" target="_blank" rel="noopener noreferrer">Timeswap Labs</a></li>
+            <li>
+              Head of Marketing and Comms at{' '}
+              <a
+                href="https://www.twitter.com/wordcel_club"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Wordcel
+              </a>
+            </li>
+            <li>
+              Member and Head of the Host Committee at the{' '}
+              <a
+                href="https://www.twitter.com/SuperteamDAO"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                SuperteamDAO
+              </a>
+            </li>
+            <li>
+              Co-founder of the{' '}
+              <a href="https://linktr.ee/slipperyslopes" target="_blank" rel="noopener noreferrer">
+                Slippery Slopes Podcast
+              </a>
+            </li>
+            <li>
+              Writing on my{' '}
+              <a
+                href="https://www.wordcelclub.com/paarug.sol"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Blog
+              </a>{' '}
+              and{' '}
+              <a href="https://twitter.com/paarugsethi" target="_blank" rel="noopener noreferrer">
+                Twitter
+              </a>
+            </li>
+            <li>
+              Previously wrote content at{' '}
+              <a href="https://twitter.com/TimeswapLabs" target="_blank" rel="noopener noreferrer">
+                Timeswap Labs
+              </a>
+            </li>
           </ul>
         </div>
         {/* <ul className="divide-y divide-gray-200 dark:divide-gray-700">
