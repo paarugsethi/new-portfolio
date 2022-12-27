27bc33d7d83eab6078775fe1be88286b27580eff
Paarug Sethi <paarug@Paarugs-MacBook-Air.local>
2022-12-27 17:11:41 +0530

    added site to metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: 'Podcaster. Wordcel. Aspiring Rotator and Marketer.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.paarug.com/',
+  siteRepo: '',
   siteLogo: '',
   image: '',
   socialBanner: '',
