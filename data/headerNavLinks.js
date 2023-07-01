5309cc6090fd1d4d1b879ffb3cf81434e24b1e89
Paarug Sethi <paarugsethi@gmail.com>
2023-07-01 13:20:58 +0530

    changed blog link

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,5 +1,5 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
+  { href: 'https://paarugsethi.substack.com', target: '_self', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
